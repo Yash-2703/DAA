e40566ac6725d70c887a0b46ed0805b247db8673
Yash <yjondhale780@gmail.com>
2023-10-06 19:50:00 +0530

    Divide And Conquer

--- Asg-1 (A-3).cpp
+++ Asg-1 (A-3).cpp
@@ -1,3 +1,11 @@
+/*
+---------------------------------------------------------------------------------------------------------------------------------------------
+Problem Statement : Implement a problem of smallest number with at least n trailing zeroes in factorial.
+                    - Given a number n. The task is to find the smallest number whose factorial contains at least n trailing zeroes.
+---------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
 #include <iostream>
 using namespace std;
 
